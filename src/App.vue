86e73285d4aee2e8c8c4986d729af296cdc84cc4
ic5594 <ic5594@naver.com>
2021-11-16 17:52:01 +0900

    Feat: 컴포넌트값 속성 및 템플릿에 연결 Style: 불필요한 스타일 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    
-  
+  <div id="app">
+    <Backgroundvid></Backgroundvid>
+    <Header></Header>
+    <Body></Body>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+<script>
+import Header from './components/header/Header.vue'
+import Backgroundvid from './Backgroundvid.vue'
+import Body from './components/body/Body.vue'
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components:{
+    'Header':Header,
+    'Backgroundvid':Backgroundvid,
+    'Body':Body
   }
 }
+</script>
+<style>
+
 </style>
