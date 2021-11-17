7eb6070c3292e4f08d84ba099e21cdd9625cdc6e
ic5594 <ic5594@naver.com>
2021-11-17 17:49:37 +0900

    Docs: router-link footer 이전으로 인한 삭제

--- src/components/body/Body.vue
+++ src/components/body/Body.vue
@@ -1,30 +1,29 @@
 <template>
-    <div id="body">
-        
+    <div id="body">  
         <ul>
-            <li><a><router-link to="/" v-on:keypress="left">TOP10</router-link></a></li>
+            <li><a>TOP10</a></li>
             <hr>
-            <li><a><router-link to="/game">게임(17)</router-link></a></li>
+            <li><a>게임(17)</a></li>
             <hr>
-            <li><a><router-link to="">만화(12)</router-link></a></li>
+            <li><a>만화(12)</a></li>
             <hr>
-            <li><a><router-link to="">키즈(4)</router-link></a>></li>
+            <li><a>키즈(4)</a>></li>
             <hr>
-            <li><a><router-link to="">이벤트</router-link></a></li>
+            <li><a>이벤트</a></li>
             <hr>
-            <li><a><router-link to="">공지사항</router-link></a></li>
+            <li><a>공지사항</a></li>
             <hr>
-            <li><a><router-link to="">고객센터</router-link></a></li>
+            <li><a>고객센터</a></li>
             <hr>
-            <li><a><router-link to="">마이페이지</router-link></a></li>
+            <li><a>마이페이지</a></li>
             <hr>
-            <li><a><router-link to="">커뮤니티</router-link></a></li>
+            <li><a>커뮤니티</a></li>
         </ul>
     </div>
 </template>
 <script>
 export default {
-    
+  
 }
 </script>
 <style>
