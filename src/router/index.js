d34feaf915d0709dafddfb336ae0f98f150fbbf9
ic5594 <ic5594@naver.com>
2021-11-17 17:50:06 +0900

    Feat: 컴포넌트 추가 및 연결을 위한 속성연결

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Top10 from '../views/list/top10/Top10.vue'
-import Game from '../views/list/game/Game.vue'
+import Top10 from '../views/top10/Top10.vue'
+import Game from '../views/game/Game.vue'
+import Kids from '../views/kids/Kids.vue'
+import Event from '../views/event/Event.vue'
+import Notice from '../views/notice/Notice.vue'
+import Service from '../views/service/Service.vue'
+import Mypage from '../views/mypage/Mypage.vue'
+import Community from '../views/community/Community.vue'
+import Footer from '../components/footer/Footer.vue'
 
 const routes = [{
         path: '/',
+        name: 'Footer',
+        component: Footer
+    },
+    {
+        path: '/top10',
         name: 'Top10',
         component: Top10
     },
@@ -11,6 +23,36 @@ const routes = [{
         path: '/game',
         name: 'Game',
         component: Game
+    },
+    {
+        path: '/kids',
+        name: 'Kids',
+        component: Kids
+    },
+    {
+        path: '/event',
+        name: 'Event',
+        component: Event
+    },
+    {
+        path: '/notice',
+        name: 'Notice',
+        component: Notice
+    },
+    {
+        path: '/service',
+        name: 'Service',
+        component: Service
+    },
+    {
+        path: '/mypage',
+        name: 'Mypage',
+        component: Mypage
+    },
+    {
+        path: '/community',
+        name: 'Community',
+        component: Community
     }
 
 ]
