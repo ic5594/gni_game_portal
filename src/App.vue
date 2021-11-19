f096f14ef5384a5b612ada5f566d89fc7e8987d8
ic5594 <ic5594@naver.com>
2021-11-19 18:01:31 +0900

    Feat: input 키모드 모듈 메서드 추가 밑 라우터배열 링크 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app" v-on:keyup.up="up" v-on:keyup.down="down" v-on:keyup.right="right" v-on:keyup.left="left">
+  <div id="app"  v-bind="number in [currentNumber]" transition="fade">
     <Backgroundvid></Backgroundvid>
     <Header></Header>
     <Body></Body>
     <router-view/>
+    <input type="button" v-on:keyup.up='up' v-on:keyup.down='down' v-on:keyup.right="right" v-on:keyup.left="left">
+    <div id="redborder" v-bind:style="redbord"></div>
   </div>
+ 
 </template>
 <script>
 import Header from './components/header/Header.vue'
@@ -20,7 +23,9 @@ export default {
   data:function(){
         return{
             routerlist:[
+            { path: '/'},
             { path:'/game'},
+            { path:'/dropdownanime'},
             { path:'/kids'},
             { path:'/event'},
             { path:'/notice'},
@@ -28,12 +33,58 @@ export default {
             { path:'/mypage'},
             { path:'/community'}],
             currentNumber:0,  
-            timer:0
-        };
+            timer:0,
+            redbord:{
+              width:'277px',
+              height: '70px',
+              border: '6px solid red',
+              position: 'absolute',
+              left:'345px',
+              top:'154px',
+              WebkitTransitionDuration: '1s',
+              MozTransitionDuration: '1s',
+              MsTransitionDuration: '1s',
+              OTransitionDuration: '1s',
+              transitionDuration: '1.5s'
+            }
+        }
     },
+    methods:{
+      up:function(){
+        console.log("up")
+        this.redbord.Transform="translateY(-0.1px)" 
+        this.currentNumber-=1
+        this.$router.push(this.routerlist[Math.abs(this.currentNumber)%this.routerlist.length])
+          },
+      down:function(){
+        console.log("down")
+        this.redbord.transform="translateY(75px)"  
+        this.currentNumber+=1
+        this.$router.push(this.routerlist[Math.abs(this.currentNumber)%this.routerlist.length])
+
+      },
+
+    }
   
 }
 </script>
 <style>
+#app input{
+  float:right;
+  background-color: white;
+  width:100px;
+  z-index: 1;
+}
+.fade-transition {
+  transition: all 2s ease;
+  overflow: hidden;
+  visibility: visible;
+  opacity: 1;
+  position: absolute;
+}
+.fade-enter, .fade-leave {
+  opacity: 0;
+  visibility: hidden;
+}
 
 </style>
