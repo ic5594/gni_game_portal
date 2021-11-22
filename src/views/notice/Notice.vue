6dc9d34fd41dd27975ae71199138a2f8dd5e4d8c
ic5594 <ic5594@naver.com>
2021-11-22 18:15:04 +0900

    Docs: no.vue 컴포넌트 연결 및 속성 연결

--- src/views/notice/Notice.vue
+++ src/views/notice/Notice.vue
@@ -1,3 +1,16 @@
 <template>
-    notice
+    <div id="notice">
+        <No></No>
+    </div>
 </template>
+<script>
+import No from '../No.vue'
+export default {
+    components:{
+        'No':No
+    }
+}
+</script>
+<style>
+
+</style>
