29c24ce26ddadf89f3d7ddd08a83ff3eaa982edf
ic5594 <ic5594@naver.com>
2021-11-22 18:14:31 +0900

    Feat: 등록 정보 x 컴포넌트 생성

---
+++ src/views/No.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="no">
+        <div class="nono">
+        <h1>등록한 정보가 없습니다.</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#no{
+    float: left;
+    width: 100px;
+    width: 300px;
+    height: 400px;
+    background-color: black;
+    opacity: 0.8;
+    margin-left: 15px;
+    margin-top: 235px;
+    border-radius: 5%;
+}
+#no h1{
+    color:white;
+    font-size: 25px;
+    text-align: center;
+    margin-top:170px;
+}
+.image {
+  position:relative;
+}
+
+</style>
