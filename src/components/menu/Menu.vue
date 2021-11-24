2d255552a3c2b61e623a731cceabba4aa3ff5185
ic5594 <ic5594@naver.com>
2021-11-24 17:41:49 +0900

    Feat: 클래스 바인딩을 통해 컴포넌트상에서 redbord값 변경 top10세부 메뉴 구현(오류 수정x)

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,25 +1,24 @@
 <template>
     <div id="body">  
         <ul>
-            <li><a>TOP10</a></li>
+            <li v-bind:class="menu1"><a>TOP10</a></li>
             <hr>
-            <li><a>게임(17)</a></li>
+            <li v-bind:class="menu2"><a>게임(17)</a></li>
             <hr>
-            <li><a>만화(12)</a></li>
+            <li v-bind:class="menu3"><a>만화(12)</a></li>
             <hr>
-            <li><a>키즈(4)</a>></li>
+            <li v-bind:class="menu4"><a>키즈(4)</a>></li>
             <hr>
-            <li><a>이벤트</a></li>
+            <li v-bind:class="menu5"><a>이벤트</a></li>
             <hr>
-            <li><a>공지사항</a></li>
+            <li v-bind:class="menu6"><a>공지사항</a></li>
             <hr>
-            <li><a>고객센터</a></li>
+            <li v-bind:class="menu7"><a>고객센터</a></li>
             <hr>
-            <li><a>마이페이지</a></li>
+            <li v-bind:class="menu8"><a>마이페이지</a></li>
             <hr>
-            <li><a>커뮤니티</a></li>
-            <div id="redborder" v-bind:style="redbord" v-if="delet"></div>
-        </ul>
+            <li v-bind:class="menu9"><a>커뮤니티</a></li>
+          </ul>
     </div>
 </template>
 <script>
@@ -37,16 +36,25 @@ export default {
             { path:'/mypage'},
             { path:'/community'},
             ],
-            currentNumber:0,  
-            redbord:{
-              left:'345px',
-              top:'153px',
-              transitionDuration: '0.5s',
-              width:'277px',
-              height:'70px',
-              border:'9px solid red',
-              position: 'absolute'
-            },
+            top10listL:[
+              {path: '/top101'},
+              {path: '/top103'}, 
+              {path: '/top105'},  
+              {path: '/top107'},
+              {path: '/top109'},
+             
+            ],
+             top10listR:[
+              {path: '/top102'},
+              {path: '/top104'},
+              {path: '/top106'},
+              {path: '/top108'},
+              {path: '/top1010'}
+            ],
+            currentNumber:0, //menu 함수 인덱스 번호
+            top10NumberL:0, //top10 함수 왼쪽 list 인덱스 번호
+            top10NumberR:0, //top10 함수 오른쪽 list 인덱스 번호
+            nextNumber:0,
             initial:0,
             amount:79,
             rightnumber:0,
@@ -54,56 +62,238 @@ export default {
 
         }
     },
+    computed:{
+      menu1:function(){
+        if(this.currentNumber%this.routerlist.length==0)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      },
+      menu2:function(){
+        if(this.currentNumber%this.routerlist.length==1)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      },
+      menu3:function(){
+        if(this.currentNumber%this.routerlist.length==2)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu4:function(){
+        if(this.currentNumber%this.routerlist.length==3)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu5:function(){
+        if(this.currentNumber%this.routerlist.length==4)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu6:function(){
+        if(this.currentNumber%this.routerlist.length==5)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu7:function(){
+        if(this.currentNumber%this.routerlist.length==6)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu8:function(){
+        if(this.currentNumber%this.routerlist.length==7)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }  ,
+      menu9:function(){
+        if(this.currentNumber%this.routerlist.length==8)
+          return{ menuredbord:true};
+        else
+          return{menuredbord:false};
+      }
+    },
     mounted(){
         window.document.onkeydown = (event)=>{
           console.log("event",event.keyCode);
 
            switch(event.keyCode){
-            case 38 :         //up
+            //up
+            case 38 :        
               this.currentNumber-=1
               if(this.currentNumber<0){
-                console.log('-')
                 this.currentNumber=this.routerlist.length-1
-                this.pushrouter()
+                this.pushrouterlist()
               }
               else{
-                this.pushrouter()
+                this.pushrouterlist()
               }
-              this.initial -=this.amount
-              this.redbordmove()
-               if(this.currentNumber%this.routerlist.length==this.routerlist.length-1){
-                 this.initial=this.amount*(this.routerlist.length-1)
-                this.redbordmove()
-              }
+              this.top10NumberL=0
+              this.top10NumberR=0
+              this.nextNumber=0
             break;
-            case 40:          //down
+
+            //down
+            case 40:          
                 this.currentNumber+=1
-                this.pushrouter()
-                this.initial +=this.amount;
-                this.redbordmove()
-                 if(this.currentNumber%this.routerlist.length==0){
-                      this.initial=0
-                     this.redbordmove()
-                  }
+                this.pushrouterlist()
+              this.top10NumberL=0
+              this.top10NumberR=0
+              this.nextNumber=0
             break;
-            case 39:
-              
-              
+
+            //right
+            case 39:  
+            this.nextNumber+=1      
+            if(this.currentNumber==0){
+                if(this.nextNumber%2==0){
+                  this.$router.push(this.top10listR[0])
+                }
+                else if(this.nextNumber%2==1){
+                  this.$router.push(this.top10listL[0])
+                }
+            }
+            
+            if(this.top10NumberL==1){
+              if(this.nextNumber%2==0){
+                this.$router.push(this.top10listR[1])
+              }
+              else if(this.nextNumber%2==1){
+                this.$router.push(this.top10listL[1])
+              }
+            }
+            if(this.top10NumberL==2){
+              if(this.nextNumber%2==0){
+                this.$router.push(this.top10listR[2])
+              }
+              else if(this.nextNumber%2==1){
+                this.$router.push(this.top10listL[2])
+              }
+            }
+            if(this.top10NumberL==3){
+              if(this.nextNumber%2==0){
+                this.$router.push(this.top10listR[3])
+              }
+              else if(this.nextNumber%2==1){
+                this.$router.push(this.top10listL[3])
+              }
+            }
+            if(this.top10NumberL==4){
+              if(this.nextNumber%2==0){
+                this.$router.push(this.top10listR[4])
+              }
+              else if(this.nextNumber%2==1){
+                this.$router.push(this.top10listL[4])
+              }
+            }
+            
             break;
-            case 37:
-              
+            
+            //left
+            case 37:    
+            if(this.currentNumber==0){
+              if(this.nextNumber>0){
+                 if(this.top10NumberL==0){
+                    if(this.nextNumber%2==1){
+                      this.$router.push(this.routerlist[0])
+                      this.nextNumber-=1    
+                   }
+                    else if(this.nextNumber%2==0){
+                      this.$router.push(this.top10listL[0])
+                      this.nextNumber-=1    
+                   }
+                   else if(this.nextNumber<0)
+                      stop()
+              }
+              }
+            }
+           
+            if(this.top10NumberL==1){
+                if(this.nextNumber%2==1){
+                  this.$router.push(this.routerlist[0])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber%2==0){
+                  this.$router.push(this.top10listL[1])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber<0)
+                  stop()
+            }
+             if(this.top10NumberL==2){
+                if(this.nextNumber%2==1){
+                  this.$router.push(this.routerlist[0])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber%2==0){
+                  this.$router.push(this.top10listL[2])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber<0)
+                  stop()
+            }
+             if(this.top10NumberL==3){
+                if(this.nextNumber%2==1){
+                  this.$router.push(this.routerlist[0])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber%2==0){
+                  this.$router.push(this.top10listL[3])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber<0)
+                  stop()
+            }
+             if(this.top10NumberL==4){
+                if(this.nextNumber%2==1){
+                  this.$router.push(this.routerlist[0])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber%2==0){
+                  this.$router.push(this.top10listL[4])
+                  this.nextNumber-=1
+                }
+                else if(this.nextNumber<0)
+                  stop()
+            }
+             //초기화
+            
             break;
+
+            //listup
+            case 33:        
+             
+            if(this.currentNumber==0){
+               this.top10NumberL-=1
+              if(this.top10NumberL<0){
+                this.top10NumberL=this.top10listL.length-1
+                this.$router.push(this.top10listL[this.top10NumberL%this.top10listL.length])
+              }
+              else{
+                this.$router.push(this.top10listL[this.top10NumberL%this.top10listL.length])
+              }
+              
+            }
+            
+            break;
+            
+            //listdown
+            case 34:        
+            this.top10NumberL+=1
+            if(this.currentNumber==0)
+                this.$router.push(this.top10listL[this.top10NumberL%this.top10listL.length])  
           }  
         }
     },
     methods:{
-      pushrouter:function(){
+      pushrouterlist:function(){
         this.$router.push(this.routerlist[this.currentNumber%this.routerlist.length]);
       },
-      redbordmove:function(){
-        this.redbord.transform="translateY(" + this.initial + "px)"
-      },
-    
     }
 }
 </script>
@@ -121,6 +311,7 @@ export default {
 }
 #body li{
     margin: 12px;
+    padding-left: 10px;
 }
 #body a{
     text-decoration-line: none;
@@ -130,12 +321,24 @@ export default {
 }
 #body hr{
     color:white;
-    margin-right: 15px;
+    margin-right: 10px;
+    margin-left: 10px;
 }
 body{
     margin: 0;
     padding: 0;
 }
-
-
+.menuredbord{
+    border:9px solid red;
+    padding: -10px;
+    width:238px;
+    height: 57px;
+    transition: opacity .5s;;
+}
+.fade-enter{
+  opacity: 1;
+}
+.fade-enter-active{
+  transition: opacity .5s;;
+}
 </style>
