fc7f9bc0de6488b57b66486d6e9b997fc3f63105
ic5594 <ic5594@naver.com>
2021-11-24 17:43:25 +0900

    Feat: redbord이동을 위한 컴포넌트 생성 및 코드구현

---
+++ src/views/Redlist.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="Redlist">
+        <h1>{{ title }}</h1>
+        <h2>장르/{{type}}<br>
+        {{about}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        title:String,
+        type:String,
+        about:String
+    }
+}
+</script>
+<style>
+#Redlist{
+    float: left;
+    width:336px;
+    background-color: black;
+    opacity: 0.7;
+    padding:5px;
+    margin: 10px;
+    height: 150px;
+    border:5px solid red;
+}
+#Redlist h1{
+    color:white;
+    margin-left: 5px;
+    margin-bottom: 3px;
+}
+#Redlist h2{
+    color:white;
+    margin-left: 5px;
+}
+</style>
---
+++ src/views/top10/Top108.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="top10">
+        <List title="스마몬마스터즈" type="퍼즐" about="퍼즐과 카드 배틀이 만났다!"></List>
+        <List title="히어로올스타즈" type="디팬스" about="최강의 히어로들이 모였다!"></List>
+        <List title="골드러너" type="캐주얼" about="보물을 찾아 떠나는 모험"></List>
+        <List title="영웅전설" type="액션RPG" about="최강의 영웅이 되자!"></List>
+        <List title="지니 신맞고" type="보드게임" about="박진감 넘치는 한판!"></List>
+        <List title="정글타워" type="캐주얼" about="하늘 높이 탑을 쌓아보자!"></List>
+        <List title="돌격정령친구들" type="캐주얼" about="최강 정령사를 가려라!"></List>
+        <Redlist title="토이레이싱" type="아케이드" about="장난감들의 레이싱 게임"></Redlist>
+        <List title="몬스터오팬스히어로" type="오팬스" about="오팬스 끝판왕! 히어로 등장"></List>      
+        <List title="치즈런" type="러닝" about="맛있는 치즈를 차지하라!"></List>  
+    </div>
+</template>
+<script>
+import List from '../list.vue'
+import Redlist from '../Redlist.vue'
+export default {
+    components:{
+    'List':List,
+    'Redlist':Redlist
+    },
+}
+</script>
+<style>
+#top10{
+    width:850px;
+    float: left;
+}
+
+</style>
