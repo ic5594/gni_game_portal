3ad1627e97f0408a79fd33d988187fa8d7318a79
ic5594 <ic5594@naver.com>
2021-11-29 17:36:45 +0900

    Feat: imageurL값 추가(데이터 바인딩)

--- src/views/kids/Kids1.vue
+++ src/views/kids/Kids1.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="kids1">
-        <Redlist title="키즈팝" type="유아교육" about="아이들의 눈높이 교육!"></Redlist>
-        <List title="타요와 신나는 놀이" type="유아" about="TV앱 강력추천! 타요!"></List>
-        <List title="판다팡" type="퍼즐" about="판다와 함께하는 3매칭 퍼즐"></List>
-        <List title="2048" type="퍼즐" about="요정과 함께하는 숫자퍼즐게임"></List>   
+        <Redlist title="키즈팝" type="유아교육" about="아이들의 눈높이 교육!" imageurL="http://61.251.167.74/webportal2018/app/20180820092125005.png"></Redlist>
+        <List title="타요와 신나는 놀이" type="유아" about="TV앱 강력추천! 타요!" imageurL="http://61.251.167.74/webportal2018/app/20180820092145350.png"></List>
+        <List title="판다팡" type="퍼즐" about="판다와 함께하는 3매칭 퍼즐" imageurL="http://61.251.167.74/webportal2018/app/20180820092046161.png"></List>
+        <List title="2048" type="퍼즐" about="요정과 함께하는 숫자퍼즐게임" imageurL="http://61.251.167.74/webportal2018/app/20180905193345771.png"></List>  
     </div>
 </template>
 <script>
