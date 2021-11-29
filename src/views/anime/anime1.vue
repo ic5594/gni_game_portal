fc1dcc9b567336f10ac194ad24e114f6c4a08449
ic5594 <ic5594@naver.com>
2021-11-29 17:38:59 +0900

    Feat: 만화 카테고리 세부 파일 생성

---
+++ src/views/anime/anime1.vue
@@ -0,0 +1,23 @@
+<template>
+    <List title="투문" about="무협/드라마"></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+    <List></List>
+</template>
+<script>
+import List from "../list.vue"
+export default {
+    components:{
+        'List':List
+    }
+}
+</script>
+<style>
+
+</style>
