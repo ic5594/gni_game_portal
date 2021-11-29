c91a1e0bb32e280a4fa11d3c010420a2947cee64
ic5594 <ic5594@naver.com>
2021-11-29 17:38:04 +0900

    Style: 왼쪽 사이드바 이미지 추가

--- src/components/leftsidebar/Leftsidebar.vue
+++ src/components/leftsidebar/Leftsidebar.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="header">
-        
+    <div id="leftside">
+        <div class="image">          
+        </div>
     </div>
 </template>
 <script>
@@ -9,14 +10,26 @@ export default {
 }
 </script>
 <style>
-#header{
+#leftside{
     float: left;
     background-color:black;
     opacity: 0.8;
     height: 100vh;
     width: 317px;
-    z-index: 1;
+    overflow:hidden;
+    margin:0 auto;
 }
+.image {
+    width: 370px;
+    height: 900px;
+    background-image: url(http://61.251.167.74/webportal2018/bnr/20210514134035259.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    border:1px solid black;
+    margin-top:30px;
+}
+
 body {
     margin: 0;
     padding: 0;
