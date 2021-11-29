dbd9cb29b80e1ddba84f8dcfa79ccc2d27ac99df
ic5594 <ic5594@naver.com>
2021-11-29 17:37:41 +0900

    Style: 배경 동영상 설정

--- src/Backgroundvid.vue
+++ src/Backgroundvid.vue
@@ -1,20 +1,37 @@
 <template>
-<div id="background">
+<div id="background" style="position: fixed; z-index: -99; width: 100%; height: 100%">
+   <iframe frameborder="0" height="100%" width="100%" 
+src="https://www.youtube.com/embed/Z432vpw-Kuc?playlist=Z432vpw-Kuc&mute=1&loop=1&autoplay=1&rel=0&controls=0&showinfo=0" allow="autoplay; encrypted-media" allowfullscreen>
 
-
-    <img src="assets/logo.png" width="100%" height="auto">
+</iframe>
 </div>
 </template>
 <style>
-img{
-    position: absolute;
-    top: 0px;
-    left: 0px;
+#background{
     min-width: 100%;
     min-height: 100%;
     width: auto;
     height: auto;
     z-index: -1;
     overflow: hidden;
+    position: relative;
+    height: 0;
+    padding-bottom: 5.25%;
+}
+html,body,div, video {
+    margin:0;padding:0;
+}
+body {
+    padding:0px, 0px, 0px, 0px; 
+    margin:0px, 0px, 0px, 0px; 
+    overflow:hidden; 
+    }
+iframe{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
+
