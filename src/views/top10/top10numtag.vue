36794cb914f7e1c0570e3844aebaae23f3c7c915
ic5594 <ic5594@naver.com>
2021-11-30 16:58:06 +0900

    Feat: top10카테고리에서 쓰이는 숫자 아이콘 컴포넌트

---
+++ src/views/top10/top10numtag.vue
@@ -0,0 +1,100 @@
+<template>
+    <img id="numtag11" src="http://61.251.167.74/ktweb/gniportal/resource/rank_01.png">
+    <img id="numtag12" src="http://61.251.167.74/ktweb/gniportal/resource/rank_02.png">
+    <img id="numtag13" src="http://61.251.167.74/ktweb/gniportal/resource/rank_03.png">
+    <img id="numtag14" src="http://61.251.167.74/ktweb/gniportal/resource/rank_04.png">
+    <img id="numtag15" src="http://61.251.167.74/ktweb/gniportal/resource/rank_05.png">
+    <img id="numtag16" src="http://61.251.167.74/ktweb/gniportal/resource/rank_06.png">
+    <img id="numtag17" src="http://61.251.167.74/ktweb/gniportal/resource/rank_07.png">
+    <img id="numtag18" src="http://61.251.167.74/ktweb/gniportal/resource/rank_08.png">
+    <img id="numtag19" src="http://61.251.167.74/ktweb/gniportal/resource/rank_09.png">
+    <img id="numtag110" src="http://61.251.167.74/ktweb/gniportal/resource/rank_10.png">
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    #numtag11{
+    position: relative;
+    bottom:940px;
+    left:10px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag12{
+    position: relative;
+    bottom:750px;
+    right:35px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag13{
+    position: relative;
+    bottom:560px;
+    right:85px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag14{
+    position: relative;
+    bottom:370px;
+    right:135px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag15{
+    position: relative;
+    bottom:178px;
+    right:185px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag16{
+    position: relative;
+    bottom:940px;
+    left: 185px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag17{
+    position: relative;
+    bottom:750px;
+    left:135px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag18{
+    position: relative;
+    bottom:560px;
+    left:90px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag19{
+    position: relative;
+    bottom:370px;
+    left:35px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+#numtag110{
+    position: relative;
+    bottom:178px;
+    right:15px;
+    z-index: 10;
+    width:50px;
+    height: 50px;
+}
+
+</style>
