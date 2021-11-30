fb3a42c598cc1351251062f62c9fd988b5ef1f1f
ic5594 <ic5594@naver.com>
2021-11-30 16:44:41 +0900

    Feat: 만화 카테고리 세부 페이지 구성

---
+++ src/views/anime/anime4.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="all">
+    <List title="나의 로맨틱 홀리데이!" type="로맨스/액션" about="최금락/최경국"></List>
+    <List title="투문" type="무협/드라마" about="이훈영/윤석준"></List>
+    <List title="지우개똥" type="일상" about="아이나무/아이나무"></List>
+    <List title="워블" type="판타지 액션" about="크래커/윤석준"></List>
+    <List title="보스 인 스쿨" type="학원 액션" about="이훈영/김의권"></List>
+    <List title="BRIGHT HOUSE" type="드라마" about="회사원/회사원"></List>
+    <List title="상수와 합정 사이" type="BL/드라마" about="봉봉/도드"></List>
+    <List title="푸드메이트" type="BL/드라마" about="토리컴즈/토리컴즈"></List>
+    <List title="어둠의 자식들" type="드라마" about="Dogaji/조세윤"></List>
+    <List title="작살무개녕" type="일상" about="냥치기/냥치기"></List>
+</div>
+</template>
+<script>
+import List from "../list.vue"
+export default {
+    components:{
+        'List':List
+    }
+}
+</script>
+<style>
+
+</style>
