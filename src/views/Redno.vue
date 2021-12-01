64f3914fe0f2a7f9020ea92e1540f222ebfc8cdd
ic5594 <ic5594@naver.com>
2021-12-01 16:24:18 +0900

    Feat: No.vue의 빨간 테투리 버젼 파일 생성

---
+++ src/views/Redno.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="redno">
+        <div class="nono">
+        <h1>등록된 정보가 없습니다.</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#redno{
+    float: left;
+    width: 100px;
+    width: 300px;
+    height: 400px;
+    background-color: black;
+    opacity: 0.8;
+    margin-left: 15px;
+    margin-top: 235px;
+    border: red 3px solid;
+    border-radius: 5%;
+}
+#redno h1{
+    color:white;
+    font-size: 25px;
+    text-align: center;
+    margin-top:170px;
+}
+.image {
+  position:relative;
+}
+
+</style>
