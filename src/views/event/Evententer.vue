134605b73718db5f2fb1e1bdecd9a16b0dc64b70
ic5594 <ic5594@naver.com>
2021-12-01 16:25:25 +0900

    Feat: 이벤트 카테고리 엔터시 나오는 컴포넌트 구현

---
+++ src/views/event/Evententer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="event">
+        <Redno></Redno>
+    </div>
+</template>
+<script>
+import Redno from '../Redno.vue'
+
+export default {
+    components:{
+        'Redno':Redno
+    }
+}
+</script>
+<style>
+
+
+</style>
