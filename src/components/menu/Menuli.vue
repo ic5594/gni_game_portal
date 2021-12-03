f46ed9dbdbef266f202e83700983c2e9237238be
ic5594 <ic5594@naver.com>
2021-12-03 17:33:16 +0900

    Feat: menu.vue 간소화를 위한 컴포넌트 파일 생성

---
+++ src/components/menu/Menuli.vue
@@ -0,0 +1,21 @@
+<template>
+    <li v-bind:class="menumove">{{menu}} </li>
+</template>
+<script>
+export default {
+    props:["menumove","menu"],
+    data:function(){
+        return{
+            
+        }
+    },
+    watch:{
+        menumove:function(number){
+            if(this.currentNumberM%this.routerlistM.length==(number))
+                return{ menuredbord:true }
+            else
+                return{ menuredbord:false }
+        }
+    }
+}
+</script>
