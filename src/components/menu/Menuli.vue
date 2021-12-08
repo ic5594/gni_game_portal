89bce36d43716dd6f0f608de51e0ce64f3360e02
ic5594 <ic5594@naver.com>
2021-12-08 17:59:02 +0900

    Docs: 불필요한 코드 삭제 및 watch 속성 삭제

--- src/components/menu/Menuli.vue
+++ src/components/menu/Menuli.vue
@@ -1,21 +1,35 @@
 <template>
-    <li v-bind:class="menumove">{{menu}} </li>
+    <div id="menuli">
+        <li>
+        {{menu}}</li>
+    </div>
 </template>
 <script>
 export default {
     props:["menumove","menu"],
     data:function(){
         return{
-            
+              menuredbord:{
+        border:'9px solid red',
+        padding:' -10px',
+        width:'238px',
+        height: '57px',
+        transition: 'opacity .5s'
+      }
         }
     },
-    watch:{
-        menumove:function(number){
-            if(this.currentNumberM%this.routerlistM.length==(number))
-                return{ menuredbord:true }
-            else
-                return{ menuredbord:false }
-        }
-    }
 }
 </script>
+<style>
+#menuli{
+    float:block;
+    color:white;
+    list-style: none;
+    text-decoration-line: none;
+    color: white;
+    font-size: 40px;
+    margin: 12px;
+    padding-left: 10px;
+}
+
+</style>
