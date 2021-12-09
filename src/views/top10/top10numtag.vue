2178d88cf2966d593af7b94c67b02f4ba6c63115
ic5594 <ic5594@naver.com>
2021-12-09 17:57:21 +0900

    Style: numtag 위치 값 변경

--- src/views/top10/top10numtag.vue
+++ src/views/top10/top10numtag.vue
@@ -9,6 +9,7 @@
     <img id="numtag18" src="http://61.251.167.74/ktweb/gniportal/resource/rank_08.png">
     <img id="numtag19" src="http://61.251.167.74/ktweb/gniportal/resource/rank_09.png">
     <img id="numtag110" src="http://61.251.167.74/ktweb/gniportal/resource/rank_10.png">
+    
 </template>
 <script>
 export default {
@@ -18,80 +19,80 @@ export default {
 <style>
     #numtag11{
     position: relative;
-    bottom:940px;
-    left:10px;
+    bottom:910px;
+    right:830px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag12{
     position: relative;
-    bottom:750px;
-    right:35px;
+    bottom:730px;
+    left:-880px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag13{
     position: relative;
-    bottom:560px;
-    right:85px;
+    bottom:540px;
+    left:-930px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag14{
     position: relative;
-    bottom:370px;
-    right:135px;
+    bottom:400px;
+    left:15px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag15{
     position: relative;
-    bottom:178px;
-    right:185px;
+    bottom:218px;
+    left:-38px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag16{
     position: relative;
-    bottom:940px;
-    left: 185px;
+    bottom:965px;
+    left: 335px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag17{
     position: relative;
-    bottom:750px;
-    left:135px;
+    bottom:780px;
+    left:285px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag18{
     position: relative;
-    bottom:560px;
-    left:90px;
+    bottom:590px;
+    left:235px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag19{
     position: relative;
-    bottom:370px;
-    left:35px;
+    bottom:410px;
+    left:185px;
     z-index: 10;
     width:50px;
     height: 50px;
 }
 #numtag110{
     position: relative;
-    bottom:178px;
-    right:15px;
+    bottom:220px;
+    left:143px;
     z-index: 10;
     width:50px;
     height: 50px;
