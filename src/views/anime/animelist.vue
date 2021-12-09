c89effd87cdf75746c991b76fe3eb4eeedb64fe5
ic5594 <ic5594@naver.com>
2021-12-09 17:57:03 +0900

    Feat: 만화 세부 컴포넌트 파일 생성 및 코드 작성

---
+++ src/views/anime/animelist.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="animelist">
+        <li>{{anime}}</li>
+    </div>
+</template>
+<script>
+export default {
+    props:["anime"]
+}
+</script>
+<style>
+#animelist li{
+  font-size: 40px;
+    color:white;
+    list-style: none;
+    margin-bottom:7px;
+}
+</style>
