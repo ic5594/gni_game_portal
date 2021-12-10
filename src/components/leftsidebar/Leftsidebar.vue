7ed53ff65d3847b429345c5bae62813fe6e6f0cb
ic5594 <ic5594@naver.com>
2021-12-10 17:34:13 +0900

    Docs: 템플릿 수정 및 네이밍 변경

--- src/components/leftsidebar/Leftsidebar.vue
+++ src/components/leftsidebar/Leftsidebar.vue
@@ -1,12 +1,25 @@
 <template>
     <div id="leftside">
-            <div class="image">          
-        </div>
+        <img src="http://61.251.167.74/webportal2018/bnr/20210514134035259.png" id="leftimg" v-bind:class="leftredbord()">        
     </div>
 </template>
 <script>
 export default {
-    props:["goNumberM"]
+    props:["leftRightNumberM","currentNumberM","routerlistM"],
+    methods:{
+        leftredbord:function(){
+            if(this.leftRightNumberM==-1){
+                return{
+                    leftredbord:true
+                }
+            }
+            else{
+                return{
+                    leftredbord:false
+                }
+            }
+        }
+    }
 }
 </script>
 <style>
@@ -19,18 +32,20 @@ export default {
     overflow:hidden;
     margin:0 auto;
 }
-.image{
-    width: 370px;
-    height: 900px;
-    background-image: url(http://61.251.167.74/webportal2018/bnr/20210514134035259.png);
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
-    border:1px solid black;
-    margin-top:30px;
+#leftimg{
+    width: 230px;
+    height: 850px;
+    position: relative;
+    top:50px;
+    left:70px;
 }
 body{
     margin: 0;
     padding: 0;
+}
+.leftredbord{
+    background-image: url(http://61.251.167.74/ktweb/gniportal/resource/popup/b_event_banner_select_01.png);
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 </style>
