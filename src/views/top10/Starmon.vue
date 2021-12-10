b4d8f8925cff441950b25ac688e210ee77951b5b
ic5594 <ic5594@naver.com>
2021-12-10 17:34:57 +0900

    Feat: 게임 라우터 링크를 위한 파일 생성

---
+++ src/views/top10/Starmon.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="starmon">
+        스마몬마스터즈 게임 페이지입니다.
+    </div>
+</template>
---
+++ src/views/top10/goldrunner.vue
@@ -0,0 +1,5 @@
+<template>
+    <div id="goldrunner">
+        골드러너 페이지입니다
+    </div>
+</template>
