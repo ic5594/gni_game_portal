e8eea760bc96359338c220dc9cfc133c410406af
ic5594 <ic5594@naver.com>
2021-12-13 17:33:02 +0900

    Feat: 라우터 링크 이동을 위한 속성 연결 및 네임드 정의

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Top10 from '../views/top10/Top10.vue'
+import Starmon from '../views/top10/Starmon.vue'
+import Goldrunner from '../views/top10/Goldrunner.vue'
+import Matgo from '../views/top10/Matgo.vue'
+import Spiritfreind from '../views/top10/Spiritfriend.vue'
+import Offensehero from '../views/top10/Offensehero.vue'
+import Heroallstarz from '../views/top10/Heroallstarz.vue'
+import Herolegend from '../views/top10/Herolegend.vue'
+import Jungletower from '../views/top10/Jungletower.vue'
+import Toyracing from '../views/top10/Toyracing.vue'
+import Cheezerun from '../views/top10/Cheezerun.vue'
 import Game from '../views/game/Game.vue'
+import Goshooter from '../views/game/Goshooter.vue'
+import Battleking from '../views/game/Battleking.vue'
+import Petmarble from '../views/game/Petmarble.vue'
 import Dropanime from '../views/anime/Dropdownanime.vue'
 import Kids from '../views/kids/Kids.vue'
 import Event from '../views/event/Event.vue'
@@ -10,15 +23,81 @@ import Mypage from '../views/mypage/Mypage.vue'
 import Community from '../views/community/Community.vue'
 
 
+
 const routes = [{
         path: '/',
         name: 'Top10',
         component: Top10
     },
     {
+        path: '/starmon',
+        name: 'Starmon',
+        component: Starmon
+    },
+    {
+        path: '/goldrunner',
+        name: 'Goldrunner',
+        component: Goldrunner
+    },
+    {
+        path: '/matgo',
+        name: 'Matgo',
+        component: Matgo
+    },
+    {
+        path: '/spiritfrined',
+        name: 'Spiritfriend',
+        component: Spiritfreind
+    },
+    {
+        path: '/offensehero',
+        name: 'Offensehero',
+        component: Offensehero
+    },
+    {
+        path: '/heroallstarz',
+        name: 'Heroallstarz',
+        component: Heroallstarz
+    },
+    {
+        path: '/herolegend',
+        name: 'Herolegend',
+        component: Herolegend
+    },
+    {
+        path: '/jungletower',
+        name: 'Jungletower',
+        component: Jungletower
+    },
+    {
+        path: '/toyracing',
+        name: 'Toyracing',
+        component: Toyracing
+    },
+    {
+        path: '/cheezerun',
+        name: 'Cheezerun',
+        component: Cheezerun
+    },
+    {
         path: '/game',
         name: 'Game',
         component: Game
+    },
+    {
+        path: '/goshooter',
+        name: 'Goshooter',
+        component: Goshooter
+    },
+    {
+        path: '/battleking',
+        name: 'Battleking',
+        component: Battleking
+    },
+    {
+        path: '/petmarble',
+        name: 'Petmarble',
+        component: Petmarble
     },
     {
         path: '/dropanime',
