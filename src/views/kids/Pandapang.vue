683893009942b98e4f070bc02d86365d4252c3a6
ic5594 <ic5594@naver.com>
2021-12-15 17:21:43 +0900

    Feat: 판다팡 메뉴 생성

---
+++ src/views/kids/Pandapang.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="pandapang">
+        <img src="../../assets/pandapang.png">
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+#pandapang{
+    z-index: 100;
+    width: 1920px;
+    height: 970px;
+    position: fixed;
+}
+#pandapang img{
+    width: 100%;
+    height: 100%;
+}
+#pandapang p{
+    color:black;   
+}
+</style>
