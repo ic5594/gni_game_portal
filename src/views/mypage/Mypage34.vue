e23f21044dc05da9eb88d675501c623cb20028b6
ic5594 <ic5594@naver.com>
2021-12-15 17:20:41 +0900

    Feat: 판다팡 컴포넌트 생성

---
+++ src/views/mypage/Mypage34.vue
@@ -0,0 +1,5 @@
+<template>
+    
+    <img src="http://61.251.167.74/ktweb/gniportal/resource/popup/b_ok_01.png" style="position: absolute; left: 558px; top: 526px;">
+    <img src="http://61.251.167.74/ktweb/gniportal/resource/popup/button_select.png" style="position: absolute; left: 550px; top: 518px;">
+</template>
