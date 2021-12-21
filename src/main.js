6481d9a262f29ede77037d821a0ab435ea94a90e
ic5594 <ic5594@naver.com>
2021-12-21 18:01:02 +0900

    Docs: axios전역설정

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+// createApp(App).use(router).mount('#app')
+axios.defaults.baseURL = 'http://localhost:8080'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.axios = axios;
+app.use(router).mount('#app')
