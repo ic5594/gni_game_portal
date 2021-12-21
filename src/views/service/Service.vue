65749e9d41b1277c9d5b0cbebd66d34d6406f478
ic5594 <ic5594@naver.com>
2021-12-21 15:35:30 +0900

    Docs: 들여쓰기

--- src/views/service/Service.vue
+++ src/views/service/Service.vue
@@ -8,13 +8,14 @@
         <p v-bind:class="serviceredbord(4,1)" style=" left:25; font-size:22px; color: rgb(238,238,238);top:203">5. 요금이 너무 많이 나왔어요! 결제내역 확인은 어떻게 하나요?</p>
         <p v-bind:class="serviceredbord(5,1)" style=" left:25; font-size:22px; color: rgb(238,238,238);top:253">6. 구매한 아이템 환불을 신청하고 싶어요. 어떻게 해야 하나요?</p>
         </div>
-        
     </div>
 </template>
 <script>
+
 export default {
     props:["leftRightNumberM","upDownNumberM","currentNumberM","routerlistM"],
     methods:{
+        
         serviceredbord:function(number1,number2){
              if(this.currentNumberM%this.routerlistM.length==6){
                 if(this.upDownNumberM%6==(number1)){
