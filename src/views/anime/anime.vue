d29751232200348d044b9db4e6b0370930c765b8
ic5594 <ic5594@naver.com>
2021-12-22 18:03:09 +0900

    Feat: 만화 세부 카테코리 컴포넌트 생성

---
+++ src/views/anime/anime.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="pupular">
+    <template v-for="(item,index) in animeData" :key="index">
+        <List v-bind="item"></List>
+    </template>
+    </div>
+</template>
+<script>
+import List from "../list.vue"
+
+export default {
+    components:{
+    'List':List
+    },
+    data:function(){
+        return{
+            animeData:[]
+        }
+    },
+    mounted:function(){
+            this.axios.get('./json/anime.json').then(response => {
+            console.log(JSON.stringify(response.data))
+            this.animeData = response.data
+        })
+    }
+}
+</script>
+<style>
+#pupular{
+    float:left;
+    width:900px;
+    position: relative;
+    left:280px;
+    bottom: 420px;
+}
+</style>
