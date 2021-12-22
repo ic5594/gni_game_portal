436923cd28a43706cab3733194b1fa22da7639f4
ic5594 <ic5594@naver.com>
2021-12-22 14:48:39 +0900

    Feat: kids 컴포넌트 데이터 비동기 통신

--- src/views/kids/Kids.vue
+++ src/views/kids/Kids.vue
@@ -1,9 +1,8 @@
 <template>
     <div id="kids">
-        <List v-bind:class="kidredbord(0,1)" title="키즈팝" type="유아교육" about="아이들의 눈높이 교육!" imageurL="http://61.251.167.74/webportal2018/app/20180820092125005.png"></List>
-        <List v-bind:class="kidredbord(1,1)" title="타요와 신나는 놀이" type="유아" about="TV앱 강력추천! 타요!" imageurL="http://61.251.167.74/webportal2018/app/20180820092145350.png"></List>
-        <List v-bind:class="kidredbord(2,1)" title="판다팡" type="퍼즐" about="판다와 함께하는 3매칭 퍼즐" imageurL="http://61.251.167.74/webportal2018/app/20180820092046161.png"></List>
-        <List v-bind:class="kidredbord(3,1)" title="2048" type="퍼즐" about="요정과 함께하는 숫자퍼즐게임" imageurL="http://61.251.167.74/webportal2018/app/20180905193345771.png"></List>   
+        <template v-for="(item,index) in kidsData" :key="index">
+            <List v-bind:class="kidsRedBord(index,1)" v-bind="item"></List>
+        </template>
     </div>
 </template>
 <script>
@@ -13,9 +12,20 @@ export default {
     components:{
     'List':List
     },
+    data:function(){
+        return{
+            kidsData:[]
+        }
+    },
     props:["leftRightNumberM","upDownNumberM","currentNumberM","routerlistM"],
+    mounted:function(){
+        this.axios.get('./kids.json').then((response) =>{
+            console.log(JSON.stringify(response.data))
+            this.kidsData = response.data
+        })
+    },
     methods:{
-        kidredbord:function(number1,number2){
+        kidsRedBord:function(number1,number2){
              if(this.currentNumberM%this.routerlistM.length==3){
                 if(this.upDownNumberM%4==(number1)){
                     if(this.leftRightNumberM==(number2)){
